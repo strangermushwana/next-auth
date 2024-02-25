9017f8a88c8ddcc6e00848d20b606a15eaba0b08
Stranger <strangermushwana@gmail.com>
2024-02-25 07:30:33 +0200

    added card wrapper

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,14 @@
+import { CardWrapper } from './CardWrapper'
+
+export default function LoginForm() {
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      LoginForm
+    </CardWrapper>
+  )
+}
